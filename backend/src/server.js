f04760f52e5f596b2209c8f161e1eaf6f573c9c0
Quang Hai <haisd01@gmail.com>
2026-01-03 11:37:56 +0700

    :art: chuan bi debloy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,32 @@ import tasksRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json()); // ⬅️ DÒNG QUAN TRỌNG
 app.use(express.urlencoded({ extended: true }));
 //midderware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use("/api/tasks", tasksRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`run server tai  localhost: ${PORT}`);
